5daa4898496551dcbecfe0ecdde38e170e896206
Romy <romy-ho@hotmail.com>
2019-01-12 13:04:25 +0100

    update

--- bubblechart.js
+++ bubblechart.js
@@ -11,8 +11,8 @@
 */
 
 window.onload = function() {
-  console.log('ja')
-  var data = $.getJSON("data.json", function(respons) {
+  // console.log('ja')
+  var data = $.getJSON("crypto-markets.json", function(respons) {
 
   var cryptoData = respons
 
@@ -53,117 +53,101 @@ window.onload = function() {
         }
       }
     }
+    bubble('2018-11-28', crypto)
       // console.log(crypto)
-// function bubble(){
+function bubble(datum,crypto){
   var diameter = 600;
   var color = d3.scaleOrdinal()
                       .domain(crypto)
                       .range(['#9e0142','#d53e4f','#f46d43','#fdae61','#fee08b','#e6f598','#abdda4','#66c2a5','#3288bd','#5e4fa']);
 
-  var bubble = d3.pack(crypto)
+  var market = []
+  for (i in crypto){
+       for (j in crypto[i].dates){
+         if(j == datum)
+             market.push([crypto[i].info.symbol,crypto[i].dates[j].market])
+           }
+         }
+
+
+  var data = {'children':market}
+
+  var bubble = d3.pack(data)
       .size([diameter, diameter])
       .padding(1.5);
 
-    console.log(crypto)
-    // console.log(typeof crypto)
   var svg = d3.select("body")
       .append("svg")
       .attr("width", diameter)
       .attr("height", diameter)
       .attr("class", "bubble");
 
-var data = []
-  for (i in crypto){
-       for (j in crypto[i]){
-         for(k in crypto[i][j]){
-           if (j == 'dates'){
-             // console.log(typeof d[i][j][k].market)
-             // console.log(d[i][j][k].market)
-            data.push(crypto[i][j][k].market)
-           }
-         }
-       }
-     }
-  var nodes = bubble.nodes({children:data})
-  .filter(function(d)
-   { for (i in d){
-        for (j in d[i]){
-          for(k in d[i][j]){
-            if (j == 'dates'){
-              // console.log(typeof d[i][j][k].market)
-              // console.log(d[i][j][k].market)
-              return !d.children;
-              console.log(d)
-            }
-          }
-        }
-      } });
+  var nodes = d3.hierarchy(data)
+                .sum(function(d){
+                      return d[1]})
 
   var node = svg.selectAll(".node")
       .data(bubble(nodes).descendants())
       .enter()
       .filter(function(d){
-        console.log(d.data)
-          return  !d.da
+          return  !d.children
       })
       .append("g")
       .attr("class", "node")
       .attr("transform", function(d) {
+        // console.log(d)
           return "translate(" + d.x + "," + d.y + ")";
       });
 
       node.append("title")
           .text(function(d) {
-            // console.log(d.data)
-            for (i in d.data){
-              // console.log(i)
-              for (j in d.data[i]){
-                // console.log(d.data[i][j])
-                if (j == 'dates'){
-                  for(k in d.data[i][j]){
-                      return i + ": " + d.data[i][j][k].market
-                  }
-                }
-            }
-          }
-
+                      return d.data[0] + ": " + d.data[1]
           });
 
-  // node.append("circle")
-  //     .attr("r", function(d) {
-  //       // console.log(d)
-  //         return d.r;
-  //     })
-  //     .style("fill", function(d,i) {
-  //         return color(i);
-  //     });
+      node.append("circle")
+          .attr("r", function(d) {
+              return d.r;
+          })
+          .style("fill", function(d,i) {
+              return color(i);
+          });
   //
-  // node.append("text")
-  //     .attr("dy", ".2em")
-  //     .style("text-anchor", "middle")
-  //     .text(function(d) {
-  //         return d.data.Name.substring(0, d.r / 3);
-  //     })
-  //     .attr("font-family", "sans-serif")
-  //     .attr("font-size", function(d){
-  //         return d.r/5;
-  //     })
-  //     .attr("fill", "white");
-  //
-  // node.append("text")
-  //     .attr("dy", "1.3em")
-  //     .style("text-anchor", "middle")
-  //     .text(function(d) {
-  //         return d.data.Count;
-  //     })
-  //     .attr("font-family",  "Gill Sans", "Gill Sans MT")
-  //     .attr("font-size", function(d){
-  //         return d.r/5;
-  //     })
-  //     .attr("fill", "white");
+  node.append("text")
+      .attr("dy", ".2em")
+      .style("text-anchor", "middle")
+      .text(function(d) {
+          return d.data[0].substring(0, d.r / 3);
+      })
+      .attr("font-family", "sans-serif")
+      .attr("font-size", function(d){
+          return d.r/5;
+      })
+      .attr("fill", "white");
+
+  node.append("text")
+      .attr("dy", "1.3em")
+      .style("text-anchor", "middle")
+      .text(function(d) {
+          return d.data[1];
+      })
+      .attr("font-family",  "Gill Sans", "Gill Sans MT")
+      .attr("font-size", function(d){
+          return d.r/5;
+      })
+      .attr("fill", "white");
 
   d3.select(self.frameElement)
       .style("height", diameter + "px");
-  // }
+
+      // scale x and y axis
+     //  var xScale = d3.scaleOrdinal()
+     //  .domain()
+     //  .range(pad);
+     //
+     // var yScale = d3.scaleLinear()
+     //    .domain([d3.min(values)-0.5, d3.max(values)+0.5])
+     //    .range([height, padding]);
+}
+
   });
 }
