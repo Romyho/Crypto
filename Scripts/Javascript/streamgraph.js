2a3566be1d391c51679071a423d3db01bb0a5d88
Romy <romy-ho@hotmail.com>
2019-01-30 18:09:44 +0100

    error d3 v2 :(

--- Scripts/Javascript/streamgraph.js
+++ Scripts/Javascript/streamgraph.js
@@ -34,8 +34,7 @@ datum = data.dates
 
 
 
-var z = d3v2.scale.ordinal()
-            .range(colorrange);
+var z = d3v2.scale.ordinal().range(colorrange);
 
 // make dictionary
   dataset = [{"key": "low", "values": []},{"key": "close", "values": []},{"key": "open", "values": []},{"key": "high", "values": []}]
